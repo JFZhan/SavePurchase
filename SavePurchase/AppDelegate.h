8dc1125a135a08dcdc5c66e138856882e1539399
JF.Zhan <jf.zhan@jfzhandemac-mini.local>
2017-06-07 18:03:57 +0800

    Initial Commit

---
+++ SavePurchase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SavePurchase
+//
+//  Created by JF.Zhan on 2017/6/7.
+//  Copyright © 2017年 JF.Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SavePurchase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SavePurchase
+//
+//  Created by JF.Zhan on 2017/6/7.
+//  Copyright © 2017年 JF.Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
